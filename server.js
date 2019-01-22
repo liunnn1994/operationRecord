af3078885a20a67b5a266a21429979d46b334804
nl <1041001122@qq.com>
2019-01-22 09:50:51 +0800

    添加查询总条数sql

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const mysql = require('./server/mysql');
-let addItem=['王五', '123.465.465.456', new Date(), '2.json', false];
+let addItem = ['王五', '123.465.465.456', new Date(), '2.json', false];
 // mysql.add(addItem).then((res) => {
 //     console.log(res);
 // });
@@ -7,10 +7,23 @@ let addItem=['王五', '123.465.465.456', new Date(), '2.json', false];
 const express = require('express');
 const path = require('path'); //系统路径模块
 const app = express();
-const port=3000;
+const port = 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/operationRecord/query', function (req, res, next) {
+  console.log(req.query);
+  const Table = 'data';
+  mysql.query(Table,1).then((dataBase) => {
+    //console.log(dataBase);
+    res.send(dataBase);
+  });
+  
+});
 
-app.listen(port, () => console.log(`服务启动成功!端口号：${port}`))
+app.post('/operationRecord/add', function (req, res) {
+  res.send('POST request to the homepage')
+});
+
+app.listen(port, () => console.log(`服务启动成功!端口号：${port}`));
 
--- server/mysql.js
+++ server/mysql.js
@@ -1,28 +1,56 @@
-
+//引入mysql组件
 const mysql = require('mysql');
+//引入mysql配置
 const databaseConfig = require('./mysql.config');
+//加载mysql配置
 var connection = mysql.createConnection(databaseConfig);
-
-
+//连接mysql
+connection.connect();
+//exports
 module.exports = {
-  query: function (Table) {
-    connection.connect();
-    var sql = `SELECT * FROM ${Table}`;
-    //查
+  /**
+   * 查询条数
+   * @param {表名} Table 
+   * @param {页码，从1开始} page 
+   * @param {每页条数，默认10条} pageSize 
+   */
+  query: function (Table, page, pageSize = 10) {
+    const start = (page - 1) * pageSize;
+    const end = start + pageSize;
+    const sql = `SELECT * FROM ${Table} limit ${start},${end}`;
+    //查询条数
+    const count = `SELECT COUNT(*) FROM ${Table}`;
+    //promise 便于调用
     const p = new Promise(function (res, rej) {
-      connection.query(sql, function (err, result) {
+      connection.query(count, function (err, totalSize) {
         if (err) {
           rej(err.message);
         } else {
-          res(result);
+          connection.query(sql, function (err, list) {
+            if (err) {
+              rej(err.message);
+            } else {
+              /**
+               * list 查询的数据
+               * totalSize 总条数
+               */
+              res({
+                list,
+                totalSize: totalSize[0]['COUNT(*)']
+              });
+            };
+          });
         };
       });
     });
-    connection.end();
+    //查
     return p;
   },
-  add: function (params=['','','','','']) {
-    connection.connect();
+  /**
+   * 
+   * @param {[操作人姓名,上传ip,上传时间,文件名,是否认为上报,]} params 
+   */
+  add: function (params = ['', '', '', '', '']) {
     var addSql = 'INSERT INTO data(Id,name,ip,date,dataFile,isReport) VALUES(0,?,?,?,?,?)';
     const p = new Promise(function (res, rej) {
       connection.query(addSql, params, function (err, result) {
@@ -33,7 +61,6 @@ module.exports = {
         };
       });
     });
-    connection.end();
     return p;
   }
 };
