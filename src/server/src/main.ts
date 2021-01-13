71316d94bdc4cd885101e50fd1ce40f7162a10f1
liu <nian.liu@keepsoft.net>
2021-01-13 17:12:03 +0800

    http框架切换为Fastify 添加swagger

--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('接口文档')
+    .setDescription('后端接口文档')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
