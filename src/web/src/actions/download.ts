4dc74e8e4622a4cd19bd3061ba708e7f355b5901
liu <nian.liu@keepsoft.net>
2021-02-02 17:26:25 +0800

    下载

---
+++ src/web/src/actions/download.ts
@@ -0,0 +1,24 @@
+export default function () {
+  const { mimeType } = this;
+  let extname = mimeType.split(";")[0][1];
+  switch (extname) {
+    case "x-matroska":
+      extname = "mkv";
+      break;
+  }
+  const now = new Date();
+  const blob = new Blob(this.recordedChunks, {
+    type: mimeType,
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style.display = "none";
+  a.href = url;
+  a.download = `${now.getFullYear()}${
+    now.getMonth() + 1
+  }${now.getDate()}${now.getTime().toString().substr(-4)}.${extname}`;
+  a.click();
+  window.URL.revokeObjectURL(url);
+  a.parentNode.removeChild(a);
+}
