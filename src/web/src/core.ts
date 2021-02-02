6c8d9718a54dcde4930ace577d242251d9b8eb2d
liu <nian.liu@keepsoft.net>
2021-02-02 17:26:16 +0800

    添加开始录制 结束录制 导出下载等方法

--- src/web/src/core.ts
+++ src/web/src/core.ts
@@ -2,6 +2,11 @@ import { checkEnv } from "./lib/index";
 import { ORInterface, iProps, FetchConfig } from "./interfaces/index";
 import startREC from "./actions/startREC";
 import stopREC from "./actions/stopREC";
+import toBlob from "./actions/toBlob";
+import toWebm from "./actions/toWebm";
+import download from "./actions/download";
+import getSupportedMimeTypes from "./actions/getSupportedMimeTypes";
+
 const { version, author, license, homepage } = require("../package.json");
 
 class OperationRecord implements ORInterface {
@@ -22,6 +27,7 @@ class OperationRecord implements ORInterface {
         video: true,
         audio: true,
       },
+      mimeType: getSupportedMimeTypes()[0],
     };
 
     if (props) {
@@ -37,6 +43,10 @@ class OperationRecord implements ORInterface {
   }
   startREC = startREC.bind(this);
   stopREC = stopREC.bind(this);
+  _toBlob = toBlob.bind(this);
+  toWebm = toWebm.bind(this);
+  _download = download.bind(this);
+  getSupportedMimeTypes = getSupportedMimeTypes.bind(this);
 }
 
 for (const [key, value] of Object.entries({
