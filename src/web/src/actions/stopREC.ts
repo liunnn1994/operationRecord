a63b87b7f59c24916571e19f7f11bb275ed6b7f7
liu <nian.liu@keepsoft.net>
2021-02-02 17:28:17 +0800

    停止录制

--- src/web/src/actions/stopREC.ts
+++ src/web/src/actions/stopREC.ts
@@ -1,9 +1,11 @@
+import { mediaDevicesErrors } from "../lib/errorStatus";
+
 export default function () {
   this.isREC = false;
   try {
     this.stream.getTracks().forEach((track: any) => track.stop());
   } catch (e) {
-    console.warn("未检测到stream", e);
+    console.warn(mediaDevicesErrors.StreamNotDetected, e);
   }
   return "停止录制";
 }
