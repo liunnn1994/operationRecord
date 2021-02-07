12d06267c0d2d0281068f0a3896bd8f0136194c3
liu <nian.liu@keepsoft.net>
2021-02-07 16:14:55 +0800

    修改默认hello world

--- src/server/src/app.controller.spec.ts
+++ src/server/src/app.controller.spec.ts
@@ -1,8 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
-describe('AppController', () => {
+describe("AppController", () => {
   let appController: AppController;
 
   beforeEach(async () => {
@@ -14,9 +14,11 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe("root", () => {
+    it("should return 'https://github.com/asdjgfr/operationRecord'", () => {
+      expect(appController.getIndex()).toBe(
+        "https://github.com/asdjgfr/operationRecord",
+      );
     });
   });
 });
