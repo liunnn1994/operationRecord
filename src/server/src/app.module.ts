9eafa4ac01844dc1b4525023414465155cfaaced
liu <nian.liu@keepsoft.net>
2021-02-07 16:03:18 +0800

    添加配置文件

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(
+      Object.assign(
+        { isGlobal: true },
+        process.env.NODE_ENV === "development"
+          ? { envFilePath: ".development.env" }
+          : {},
+      ),
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+} from "@nestjs/platform-fastify";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -12,12 +12,14 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   const swaggerOptions = new DocumentBuilder()
-    .setTitle('接口文档')
-    .setDescription('后端接口文档')
-    .setVersion('1.0')
+    .setTitle("接口文档")
+    .setDescription("后端接口文档")
+    .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, swaggerOptions);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  SwaggerModule.setup("api", app, document);
+  await app.listen(8990);
 }
-bootstrap();
+bootstrap().then(() => {
+  console.log("项目启动成功，端口：", process.env);
+});
