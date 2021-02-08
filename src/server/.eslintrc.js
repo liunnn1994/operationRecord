5f24167dd446d8fd0c450e1ee36cebc0d01bf1f3
liu <nian.liu@keepsoft.net>
2021-02-08 11:33:26 +0800

    重置server

--- src/server/.eslintrc.js
+++ src/server/.eslintrc.js
@@ -1,34 +1,25 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "tsconfig.json",
-    sourceType: "module",
+    project: 'tsconfig.json',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint/eslint-plugin"],
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: [".eslintrc.js"],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "prettier/prettier": ["error", { singleQuote: false }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
-  overrides: [
-    {
-      files: ["*"],
-      rules: {
-        quotes: [2, "double"],
-      },
-    },
-  ],
 };
--- src/server/src/main.ts
+++ src/server/src/main.ts
@@ -1,27 +1,8 @@
 import { NestFactory } from "@nestjs/core";
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-import { WsAdapter } from "@nestjs/platform-ws";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  // 初始化文档
-  const swaggerOptions = new DocumentBuilder()
-    .setTitle("接口文档")
-    .setDescription("后端接口文档")
-    .setVersion("1.0")
-    .build();
-  const document = SwaggerModule.createDocument(app, swaggerOptions);
-  SwaggerModule.setup("api", app, document);
-  // 初始化ws
-  app.useWebSocketAdapter(new WsAdapter(app));
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT);
 }
 bootstrap().then(() => {
