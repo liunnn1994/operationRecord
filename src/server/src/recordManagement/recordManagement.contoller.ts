b27ba69eae799370a6ef032f5756ed5662612154
DESKTOP-P0E4VSH\win <liunian@2077tech.com>
2021-02-16 12:47:38 +0800

    支持批量查询

--- src/server/src/recordManagement/recordManagement.contoller.ts
+++ src/server/src/recordManagement/recordManagement.contoller.ts
@@ -10,7 +10,7 @@ export class RecordManagementController {
     private readonly recordManagementService: RecordManagementService,
   ) {}
 
-  @Post("/get-record-by-id")
+  @Post("/get-record-by-ids")
   async getRecordByID(
     @Body() body: RecordManagementFindByPKDto,
   ): Promise<ResInterface> {
@@ -29,6 +29,19 @@ export class RecordManagementController {
       code: HttpStatusCode.OK,
       data: await this.recordManagementService.findAll(),
       message: "查找成功",
+    };
+  }
+  @Post("/remove-records-by-ids")
+  async removeRecordsByIDs(
+    @Body() body: RecordManagementFindByPKDto,
+  ): Promise<ResInterface> {
+    const res = await this.recordManagementService.remove(body.id);
+    return {
+      code: res.success
+        ? HttpStatusCode.OK
+        : HttpStatusCode.INTERNAL_SERVER_ERROR,
+      data: body.id,
+      message: res.message,
     };
   }
 }
